22670113094abaa6d9683f6a6e6865ace6ddcee8
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-25 14:44:47 +0100

    extension completed and tested

--- models/cinema.js
+++ models/cinema.js
@@ -23,5 +23,40 @@ Cinema.prototype.byGenre = function(genre) {
   return films
 }
 
+Cinema.prototype.filmByYear = function(year) {
+  const films = this.films.some((film) => {
+    return film.year == year
+  })
+  return films
+}
+
+Cinema.prototype.overLength = function(length) {
+  const films = this.films.every((film) => {
+    return film.length >= length;
+  })
+  return films
+}
+
+Cinema.prototype.totalRunning = function() {
+  const total = this.films.reduce((runningTotal, filmLength) => {
+    return runningTotal + filmLength.length
+  }, 0)
+  return total
+}
+
+Cinema.prototype.filter = function(category, value) {
+  const films = this.films.filter((film) => {
+    if (category == "title") {
+      return film.title == value;
+    } else if (category == "genre") {
+      return film.genre == value;
+    } else if (category == "length") {
+      return film.length == value;
+    } else {
+      return film.year == value;
+    }
+  })
+  return films
+}
 
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -46,14 +46,33 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, expected)
   });
 
+  it('should be able to check whether there are some films from a particular year', function() {
+    const actual = cinema.filmByYear(2017);
+    const expected = true;
+    assert.strictEqual(actual, expected);
+  });
+  
+  it('should be able to check whether there are no films from a particular year', function() {
+    const actual = cinema.filmByYear(2002);
+    const expected = false;
+    assert.strictEqual(actual, expected);
+  });
+  
+  it('should be able to check whether all films are over a particular length', function() {
+    const actual = cinema.overLength(60);
+    const expected = true;
+    assert.strictEqual(actual, expected);
+  });
+  
+  it('should be able to calculate total running time of all films', function() {
+    const actual = cinema.totalRunning();
+    const expected = 622
+    assert.strictEqual(actual, expected);
+  });
 
-
-
-
-
-  it('should be able to check whether there are some films from a particular year');
-  it('should be able to check whether there are no films from a particular year');
-  it('should be able to check whether all films are over a particular length');
-  it('should be able to calculate total running time of all films');
-
+  it ('Cinema should be able to filter films by year', function() {
+    const actual = cinema.filter("year", 2017);
+    const expected = [bladeRunner, dunkirk, trainspotting];
+    assert.deepStrictEqual(actual, expected);
+  })
 });
